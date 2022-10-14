80c1525973e7f0bb1266feb63d9c9250e5f9390d
DavideB45 <dadewww@gmail.com>
2022-10-14 20:36:12 +0200

    completed hello world

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,10 +1,9 @@
-from keras import mnist
+from keras.datasets import mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
 # Jist print the format of the dataset
-train_images.shape
-len(train_labels)
-train_labels
+print(train_images.shape)
+print(train_labels)
 
 from keras import models
 from keras import layers
@@ -12,3 +11,21 @@ network = models.Sequential()
 network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
 #softmax is a layer in wich the sum of every output sums to 1
 network.add(layers.Dense(10, activation='softmax'))
+
+
+network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype('float32') / 255
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype('float32') / 255
+
+from keras.utils import to_categorical
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+
+network.fit(train_images, train_labels, epochs=5, batch_size=128)
+
+test_loss, test_acc = network.evaluate(test_images, test_labels)
+print('test_acc:', test_acc)
