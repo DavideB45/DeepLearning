73679e1e60e19f26c0709f55fc36e6aae9039540
DavideB45 <dadewww@gmail.com>
2022-10-25 09:48:11 +0200

    InOut preparation

---
+++ YOLOv2_implementation/InOut.py
@@ -0,0 +1,409 @@
+import cv2
+from tensorflow_hub import image_embedding_column
+from AnchorBoxClustering import parse_annotation
+import numpy as np
+import matplotlib.pyplot as plt
+
+LABELS = ['dispositivo']
+
+train_img = 'YOLOv2_implementation/training/img'
+train_ann = 'YOLOv2_implementation/training/ann'
+
+valid_img = 'YOLOv2_implementation/validation/img'
+valid_ann = 'YOLOv2_implementation/validation/ann'
+
+test_img = 'YOLOv2_implementation/test/img'
+test_ann = 'YOLOv2_implementation/test/ann'
+
+np.random.seed(10)
+train_image, seen_train_labels = parse_annotation(train_ann,
+                                                  train_img, 
+                                                  labels=LABELS)
+print("N train = {}".format(len(train_image)))
+
+
+###################################################################################
+############################## IMAGE FORMATTER ####################################
+###################################################################################
+# l'input è pretty simple basta prendere un'immagine 
+# e renderla di una forma pre specificata
+# la classe ImageReader fa proprio questo:
+import copy
+class ImageReader(object):
+    def __init__(self, IMAGE_H, IMAGE_W, norm=None) -> None:
+        '''
+        IMAGE_H: l'altezza dell'immagine riscalata
+        IMAGE_W: la largehzza dell'immagine riscalata
+        '''
+        self.IMAGE_H = IMAGE_H
+        self.IMAGE_W = IMAGE_W
+        self.norm = norm
+
+    def encode_core(self, image, reorder_rgb=True):
+        #resize the image to standard size
+        image = cv2.resize(image, (self.IMAGE_H, self.IMAGE_W))
+        if reorder_rgb:
+            image = image[:,:,::-1]
+        if self.norm is not None:
+            image = self.norm(image)
+        return(image)
+    
+    def fit(self, train_istance):
+        '''
+        read in and resize the image, annotations are resized accordingly.
+        
+        -- Input -- 
+        
+        train_instance : dictionary containing filename, height, width and object
+        
+        {'filename': 'ObjectDetectionRCNN/VOCdevkit/VOC2012/JPEGImages/2008_000054.jpg',
+         'height':   333,
+         'width':    500,
+         'object': [{'name': 'bird',
+                     'xmax': 318,
+                     'xmin': 284,
+                     'ymax': 184,
+                     'ymin': 100},
+                    {'name': 'bird', 
+                     'xmax': 198, 
+                     'xmin': 112, 
+                     'ymax': 209, 
+                     'ymin': 146}]
+        }
+        '''
+        if not isinstance(train_istance, dict):
+            train_istance = {'filename':train_istance}
+        image_name = train_istance['filename']
+        image = cv2.imread(image_name)
+        h, w, c = image.shape
+        if image is None: print('Cannot find ', image_name)
+        image = self.encode_core(image, reorder_rgb=True)
+        if "object" in train_istance.keys():
+            all_objs = copy.deepcopy(train_istance['object'])
+            #fix object position and size
+            for obj in all_objs:
+                for attr in ['xmin', 'xmax']:
+                    obj[attr] = int(obj[attr]* float(self.IMAGE_W)/w)
+                    obj[attr] = max(min(obj[attr], self.IMAGE_W), 0)
+                for attr in ['ymin', 'ymax']:
+                    obj[attr] = int(obj[attr] * float(self.IMAGE_H) / h)
+                    obj[attr] = max(min(obj[attr], self.IMAGE_H), 0)
+        else:
+            return image
+        return image, all_objs
+
+def imgNormalize(image):
+    return image / 255.0
+
+
+
+###################################################################################
+############################ ANCHOR BOX FINDER ####################################
+###################################################################################
+# class used to find the best bounding box nel senso: quello che 
+# si adatta meglio alla forma della "ground truth"
+class BestAnchorBoxFinder(object):
+    def __init__(self, ANCHORS):
+        '''
+        ANCHORS: a np.array of even number length e.g.
+        
+        _ANCHORS = [4,2, ##  width=4, height=2,  flat large anchor box
+                    2,4, ##  width=2, height=4,  tall large anchor box
+                    1,1] ##  width=1, height=1,  small anchor box
+        '''
+        self.anchors = [BoundBox(0, 0, ANCHORS[2*i], ANCHORS[2*i+1]) 
+                        for i in range(int(len(ANCHORS)//2))]
+        
+    def _interval_overlap(self,interval_a, interval_b):
+        x1, x2 = interval_a
+        x3, x4 = interval_b
+        if x3 < x1:
+            if x4 < x1:
+                return 0
+            else:
+                return min(x2,x4) - x1
+        else:
+            if x2 < x3:
+                 return 0
+            else:
+                return min(x2,x4) - x3  
+
+    def bbox_iou(self,box1, box2):
+        intersect_w = self._interval_overlap([box1.xmin, box1.xmax], [box2.xmin, box2.xmax])
+        intersect_h = self._interval_overlap([box1.ymin, box1.ymax], [box2.ymin, box2.ymax])  
+
+        intersect = intersect_w * intersect_h
+
+        w1, h1 = box1.xmax-box1.xmin, box1.ymax-box1.ymin
+        w2, h2 = box2.xmax-box2.xmin, box2.ymax-box2.ymin
+
+        union = w1*h1 + w2*h2 - intersect
+
+        return float(intersect) / union
+    
+    def find(self,center_w, center_h):
+        # find the anchor that best predicts this box
+        best_anchor = -1
+        max_iou     = -1
+        # each Anchor box is specialized to have a certain shape.
+        # e.g., flat large rectangle, or small square
+        shifted_box = BoundBox(0, 0,center_w, center_h)
+        ##  For given object, find the best anchor box!
+        for i in range(len(self.anchors)): ## run through each anchor box
+            anchor = self.anchors[i]
+            iou    = self.bbox_iou(shifted_box, anchor)
+            if max_iou < iou:
+                best_anchor = i
+                max_iou     = iou
+        return(best_anchor,max_iou)    
+    
+    
+class BoundBox:
+    def __init__(self, xmin, ymin, xmax, ymax, confidence=None,classes=None):
+        self.xmin, self.ymin = xmin, ymin
+        self.xmax, self.ymax = xmax, ymax
+        ## the code below are used during inference
+        # probability
+        self.confidence      = confidence
+        # class probaiblities [c1, c2, .. cNclass]
+        self.set_class(classes)
+    def set_class(self,classes):
+        self.classes = classes
+        self.label   = np.argmax(self.classes) 
+    def get_label(self):  
+        return(self.label)
+    def get_score(self):
+        return(self.classes[self.label])
+
+
+###################################################################################
+############################## OUTPUT ENCODING ####################################
+###################################################################################
+# l'output deve essere un valore relativo al numero di celle,
+# ma noi lo stiamo esprimeno in numero di pixel, quindi scriviamo alcune funzioni
+# che ci permetteranno di convertire i nostri bounding box presenti nei dati
+# in modo da poterli confrontare
+def rescale_centerxy(obj,config):
+    '''
+    obj:     dictionary containing xmin, xmax, ymin, ymax
+    config : dictionary containing IMAGE_W, GRID_W, IMAGE_H and GRID_H
+    '''
+    center_x = .5*(obj['xmin'] + obj['xmax'])
+    center_x = center_x / (float(config['IMAGE_W']) / config['GRID_W'])
+    center_y = .5*(obj['ymin'] + obj['ymax'])
+    center_y = center_y / (float(config['IMAGE_H']) / config['GRID_H'])
+    return(center_x,center_y)
+
+def rescale_cebterwh(obj,config):
+    '''
+    obj:     dictionary containing xmin, xmax, ymin, ymax
+    config : dictionary containing IMAGE_W, GRID_W, IMAGE_H and GRID_H
+    '''    
+    # unit: grid cell
+    center_w = (obj['xmax'] - obj['xmin']) / (float(config['IMAGE_W']) / config['GRID_W']) 
+    # unit: grid cell
+    center_h = (obj['ymax'] - obj['ymin']) / (float(config['IMAGE_H']) / config['GRID_H']) 
+    return(center_w,center_h)
+
+
+###################################################################################
+############################## BATCH GENERATOR ####################################
+###################################################################################
+# ci serve perché in questo modo le immagini saranno prese a blocchi
+# e questo è necessario per ottimizzare l'utilizzo della memoria
+# inoltre ci permetterà di introdurre della data augmentation
+from keras.utils import Sequence
+
+class SimpleBatchGenerator(Sequence):
+    def __init__(self, images, config, norm=None, shuffle=True) -> None:
+        '''
+        config : dictionary containing necessary hyper parameters for traning. e.g., 
+            {
+            'IMAGE_H'         : 416, 
+            'IMAGE_W'         : 416,
+            'GRID_H'          : 13,  
+            'GRID_W'          : 13,
+            'LABELS'          : ['aeroplane',  'bicycle', 'bird',  'boat',      'bottle', 
+                                  'bus',        'car',      'cat',  'chair',     'cow',
+                                  'diningtable','dog',    'horse',  'motorbike', 'person',
+                                  'pottedplant','sheep',  'sofa',   'train',   'tvmonitor'],
+            'ANCHORS'         : array([ 1.07709888,   1.78171903,  
+                                        2.71054693,   5.12469308, 
+                                        10.47181473, 10.09646365,  
+                                        5.48531347,   8.11011331]),
+            'BATCH_SIZE'      : 16,
+            'TRUE_BOX_BUFFER' : 50,
+            }
+        '''
+        self.config = config
+        self.config["BOX"] = int(len(self.config['ANCHORS'])/2)
+        self.config["CLASS"] = len(self.config['LABELS'])
+        self.images = images
+        self.bestAnchorBoxFinder = BestAnchorBoxFinder(config['ANCHORS'])
+        self.imageReader = ImageReader(config['IMAGE_H'],config['IMAGE_W'],norm=norm)
+        self.shuffle = shuffle
+        if self.shuffle: 
+            np.random.shuffle(self.images)
+        
+    def __len__(self):
+        return int(np.ceil(float(len(self.images))/self.config['BATCH_SIZE']))  
+
+    def __getitem__(self, idx):
+        '''
+        == input == 
+        idx : non-negative integer value e.g., 0
+        
+        == output ==
+        x_batch: The numpy array of shape  (BATCH_SIZE, IMAGE_H, IMAGE_W, N channels).
+            x_batch[iframe,:,:,:] contains a iframeth frame of size  (IMAGE_H,IMAGE_W).
+        y_batch:
+            The numpy array of shape  (BATCH_SIZE, GRID_H, GRID_W, BOX, 4 + 1 + N classes). 
+            BOX = The number of anchor boxes.
+        b_batch:
+            The numpy array of shape (BATCH_SIZE, 1, 1, 1, TRUE_BOX_BUFFER, 4).
+        '''
+        l_bound = idx*self.config['BATCH_SIZE']
+        r_bound = (idx+1)*self.config['BATCH_SIZE']
+        if r_bound > len(self.images):
+            r_bound = len(self.images)
+            l_bound = r_bound - self.config['BATCH_SIZE']
+        instance_count = 0    
+        # prepare empty storage space: this will be output
+        x_batch = np.zeros((r_bound - l_bound, self.config['IMAGE_H'], self.config['IMAGE_W'], 3)) # input images
+        b_batch = np.zeros((r_bound - l_bound, 1     , 1     , 1    ,  self.config['TRUE_BOX_BUFFER'], 4)) # list of self.config['TRUE_self.config['BOX']_BUFFER'] GT boxes
+        y_batch = np.zeros((r_bound - l_bound, self.config['GRID_H'],  self.config['GRID_W'], self.config['BOX'], 4+1+len(self.config['LABELS']))) # desired network output
+        for train_instance in self.images[l_bound:r_bound]:
+            # augment input image and fix object's position and size
+            img, all_objs = self.imageReader.fit(train_instance)
+            # construct output from object's x, y, w, h
+            true_box_index = 0
+            for obj in all_objs:
+                if obj['xmax'] > obj['xmin'] and obj['ymax'] > obj['ymin'] and obj['name'] in self.config['LABELS']:
+                    center_x, center_y = rescale_centerxy(obj,self.config)
+                    grid_x = int(np.floor(center_x))
+                    grid_y = int(np.floor(center_y))
+                    if grid_x < self.config['GRID_W'] and grid_y < self.config['GRID_H']:
+                        obj_indx  = self.config['LABELS'].index(obj['name'])
+                        center_w, center_h = rescale_cebterwh(obj,self.config)
+                        box = [center_x, center_y, center_w, center_h]
+                        best_anchor,max_iou = self.bestAnchorBoxFinder.find(center_w, center_h)
+                        # assign ground truth x, y, w, h, confidence and class probs to y_batch
+                        # it could happen that the same grid cell contain 2 similar shape objects
+                        # as a result the same anchor box is selected as the best anchor box by the multiple objects
+                        # in such ase, the object is over written
+                        y_batch[instance_count, grid_y, grid_x, best_anchor, 0:4] = box # center_x, center_y, w, h
+                        y_batch[instance_count, grid_y, grid_x, best_anchor, 4  ] = 1. # ground truth confidence is 1
+                        y_batch[instance_count, grid_y, grid_x, best_anchor, 5+obj_indx] = 1 # class probability of the object
+                        # assign the true box to b_batch
+                        b_batch[instance_count, 0, 0, 0, true_box_index] = box
+                        true_box_index += 1
+                        true_box_index = true_box_index % self.config['TRUE_BOX_BUFFER']
+            x_batch[instance_count] = img
+            # increase instance counter in current batch
+            instance_count += 1  
+        return [x_batch, b_batch], y_batch
+
+    def on_epoch_end(self):
+        if self.shuffle: 
+            np.random.shuffle(self.images)
+
+# vediamo un esempio di come si usa questa classe:
+GRID_H,  GRID_W  = 13 , 13
+ANCHORS          = [ 1.07709888,   1.78171903,  
+                    2.71054693,   5.12469308, 
+                    10.47181473, 10.09646365,  
+                    5.48531347,   8.11011331]
+ANCHORS[::2]     = ANCHORS[::2]*GRID_W  
+ANCHORS[1::2]    = ANCHORS[1::2]*GRID_H  
+print(ANCHORS)
+IMAGE_H, IMAGE_W = 416, 416
+BATCH_SIZE       = 16
+TRUE_BOX_BUFFER  = 50
+generator_config = {
+    'IMAGE_H'         : IMAGE_H, 
+    'IMAGE_W'         : IMAGE_W,
+    'GRID_H'          : GRID_H,  
+    'GRID_W'          : GRID_W,
+    'LABELS'          : LABELS,
+    'ANCHORS'         : ANCHORS,
+    'BATCH_SIZE'      : BATCH_SIZE,
+    'TRUE_BOX_BUFFER' : TRUE_BOX_BUFFER,
+}
+train_batch_generator = SimpleBatchGenerator(train_image, generator_config,
+                                             norm=imgNormalize, shuffle=True)
+[x_batch,b_batch],y_batch = train_batch_generator.__getitem__(idx=3)
+print("x_batch: (BATCH_SIZE, IMAGE_H, IMAGE_W, N channels)           = {}".format(x_batch.shape))
+print("y_batch: (BATCH_SIZE, GRID_H, GRID_W, BOX, 4 + 1 + N classes) = {}".format(y_batch.shape))
+print("b_batch: (BATCH_SIZE, 1, 1, 1, TRUE_BOX_BUFFER, 4)            = {}".format(b_batch.shape))
+
+# vediamo quali griglie sono scelte per lgi oggetti in un'immagine:
+iframe= 1
+def check_object_in_grid_anchor_pair(irow):
+    for igrid_h in range(generator_config["GRID_H"]):
+        for igrid_w in range(generator_config["GRID_W"]):
+            for ianchor in range(generator_config["BOX"]):
+                vec = y_batch[irow,igrid_h,igrid_w,ianchor,:]
+                C = vec[4] ## ground truth confidence
+                if C == 1:
+                    class_nm = np.array(LABELS)[np.where(vec[5:])]
+                    assert len(class_nm) == 1
+                    print("igrid_h={:02.0f},igrid_w={:02.0f},iAnchor={:02.0f}, {}".format(
+                        igrid_h,igrid_w,ianchor,class_nm[0]))
+check_object_in_grid_anchor_pair(iframe)
+
+# ora vediamo come sono nell'immagine
+def plot_image_with_grid_cell_partition(irow):
+    img = x_batch[irow]
+    plt.figure(figsize=(15,15))
+    plt.imshow(img)
+    for wh in ["W","H"]:
+        GRID_       = generator_config["GRID_"  + wh] ## 13
+        IMAGE_      = generator_config["IMAGE_" + wh] ## 416
+        if wh == "W":
+            pltax   = plt.axvline
+            plttick = plt.xticks
+        else:
+            pltax   = plt.axhline
+            plttick = plt.yticks
+            
+        for count in range(GRID_):
+            l = IMAGE_*count/GRID_
+            pltax(l,color="yellow",alpha=0.3) 
+        plttick([(i + 0.5)*IMAGE_/GRID_ for i in range(GRID_)],
+                ["iGRID{}={}".format(wh,i) for i in range(GRID_)])
+
+def plot_grid(irow):
+    import seaborn as sns
+    color_palette = list(sns.xkcd_rgb.values())
+    iobj = 0
+    for igrid_h in range(generator_config["GRID_H"]):
+        for igrid_w in range(generator_config["GRID_W"]):
+            for ianchor in range(generator_config["BOX"]):
+                vec = y_batch[irow,igrid_h,igrid_w,ianchor,:]
+                C = vec[4] ## ground truth confidence
+                if C == 1:
+                    class_nm = np.array(LABELS)[np.where(vec[5:])]
+                    x, y, w, h = vec[:4]
+                    multx = generator_config["IMAGE_W"]/generator_config["GRID_W"]
+                    multy = generator_config["IMAGE_H"]/generator_config["GRID_H"]
+                    c = color_palette[iobj]
+                    iobj += 1
+                    xmin = x - 0.5*w
+                    ymin = y - 0.5*h
+                    xmax = x + 0.5*w
+                    ymax = y + 0.5*h
+                    # center
+                    plt.text(x*multx,y*multy,
+                             "X",color=c,fontsize=23)
+                    plt.plot(np.array([xmin,xmin])*multx,
+                             np.array([ymin,ymax])*multy,color=c,linewidth=10)
+                    plt.plot(np.array([xmin,xmax])*multx,
+                             np.array([ymin,ymin])*multy,color=c,linewidth=10)
+                    plt.plot(np.array([xmax,xmax])*multx,
+                             np.array([ymax,ymin])*multy,color=c,linewidth=10)  
+                    plt.plot(np.array([xmin,xmax])*multx,
+                             np.array([ymax,ymax])*multy,color=c,linewidth=10)
+
+plot_image_with_grid_cell_partition(iframe)
+plot_grid(iframe)
+plt.show()
